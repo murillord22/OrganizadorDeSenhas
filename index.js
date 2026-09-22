b60388bae6c7a9878e6b6cfc1a0bbe48504ddaee
Murilo Rodrigues <22rdfe@gmail.com>
2026-09-22 16:32:14 -0300

    Criar index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const usuario = "antonio.silva4721@aluno.ce.gov.br"
+const senha = "@EEEp.2026"
+
+let usuarioDigitado = "antonio.silva4721"
+let senhaDigitada = "@EEEp.2026"
+
+if (!usuarioDigitado || !senhaDigitada) {
+  console.log("Preencha todos os campos")
+} else if (usuarioDigitado === usuario && senhaDigitada === senha) {
+  console.log("Login realizado com sucesso")
+} else {
+  console.log("Usuário ou senha incorretos")
+}
